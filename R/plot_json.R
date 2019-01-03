de24b8359a895e8e549b8903bbe896e2c62f869d
Roberto Preste <roberto.preste@gmail.com>
2019-01-03 16:25:46 +0100

    Working on plot_json function

--- R/plot_json.R
+++ R/plot_json.R
@@ -4,6 +4,8 @@
 #' mitochondrial variants contained in the given JSON file
 #'
 #' @param json_file Path of the input JSON file
+#' @param json_format JSON format used; allowed values are c("vector", "dataframe") (default = "vector")
+#' @param var_col When json_format = "dataframe", name of column containing variants position (default = "POS")
 #' @param show_loci_names Show labels of loci names on the genome (default = TRUE)
 #' @param show_loci_legend Show legend for locus type colours (default = TRUE)
 #' @param title Set plot title (default = "")
@@ -12,8 +14,14 @@
 #'
 #' @return p ggplot2 polar plot variants on the human mitochondrial genome
 #'
+#' @import jsonlite
 #' @import ggplot2
 #'
 #' @export
 
+plot_json <- function(json_file, json_format = "vector", var_col = "POS",
+                      show_loci_names = TRUE, show_loci_legend = TRUE,
+                      title = "", save_plot = FALSE, save_to = "mitoviz_plot.png") {
 
+
+}
