9664e4818186032db6b9757907127266955ed3a4
Roberto Preste <roberto.preste@gmail.com>
2019-01-03 15:52:57 +0100

    Remove useless verbose option

--- R/plot_df.R
+++ R/plot_df.R
@@ -9,7 +9,6 @@
 #' @param title Set plot title (default = "")
 #' @param save_plot Save the current plot to a file (default = FALSE)
 #' @param save_to Path or filename to which the current plot will be saved (default = "mitoviz_plot.png")
-#' @param verbose Show messages returned by the \code{vcfR} package functions (default = FALSE)
 #'
 #' @return p ggplot2 polar plot variants on the human mitochondrial genome
 #'
@@ -18,8 +17,7 @@
 #' @export
 
 plot_df <- function(dataframe, show_loci_names = TRUE, show_loci_legend = TRUE,
-                    title = "", save_plot = FALSE, save_to = "mitoviz_plot.png",
-                    verbose = FALSE) {
+                    title = "", save_plot = FALSE, save_to = "mitoviz_plot.png") {
     dataframe$POS <- as.numeric(dataframe$POS)
     p <- mito_plot(show_loci_names = show_loci_names,
                    show_loci_legend = show_loci_legend) +
