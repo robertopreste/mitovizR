a0ce676b0c5121ef271c4c88a3843f1c4b81317d
Roberto Preste <roberto.preste@gmail.com>
2019-01-14 18:16:23 +0100

    Update tests for mito_plot() hoping to avoid tar issue

--- tests/testthat/test-mito_plot.R
+++ tests/testthat/test-mito_plot.R
@@ -2,20 +2,20 @@ context("Creating basic mitochondrial plot")
 
 test_that("mito_plot returns correct plot without arguments", {
     p <- mito_plot()
-    vdiffr::expect_doppelganger("base plot no arguments", p)
+    vdiffr::expect_doppelganger("no args", p)
 })
 
 test_that("mito_plot returns correct plot with show_loci_names = FALSE", {
     p <- mito_plot(show_loci_names = FALSE)
-    vdiffr::expect_doppelganger("base plot show_loci_names FALSE", p)
+    vdiffr::expect_doppelganger("show_loci_names F", p)
 })
 
 test_that("mito_plot returns correct plot with show_loci_legend = FALSE", {
     p <- mito_plot(show_loci_legend = FALSE)
-    vdiffr::expect_doppelganger("base plot show_loci_legend FALSE", p)
+    vdiffr::expect_doppelganger("show_loci_legend F", p)
 })
 
 test_that("mito_plot returns correct plot with show_loci_names = FALSE show_loci_legend = FALSE", {
     p <- mito_plot(show_loci_names = FALSE, show_loci_legend = FALSE)
-    vdiffr::expect_doppelganger("base plot show_loci_names FALSE show_loci_legend FALSE", p)
+    vdiffr::expect_doppelganger("show_loci_names F show_loci_legend F", p)
 })
