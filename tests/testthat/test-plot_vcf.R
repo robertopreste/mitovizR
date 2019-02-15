1c45ef8cdf56d1fa0cb182970221fbb3270bf170
Roberto Preste <roberto.preste@gmail.com>
2019-02-15 17:40:45 +0100

    Add tests for plotting multiple samples from VCF files

--- tests/testthat/test-plot_vcf.R
+++ tests/testthat/test-plot_vcf.R
@@ -14,3 +14,18 @@ test_that("plot_vcf returns correct plot with show_loci_legend = FALSE", {
     p <- plot_vcf("HG00119.vcf", show_loci_legend = FALSE)
     vdiffr::expect_doppelganger("show_loci_legend F", p)
 })
+
+test_that("plot_vcf returns correct plot from multiple samples", {
+    p <- plot_vcf("HG00119_multi.vcf")
+    vdiffr::expect_doppelganger("multi no args", p)
+})
+
+test_that("plot_vcf returns correct plot from multiple sample with show_loci_names = FALSE", {
+    p <- plot_vcf("HG00119_multi.vcf", show_loci_names = F)
+    vdiffr::expect_doppelganger("multi show_loci_names F", p)
+})
+
+test_that("plot_vcf returns correct plot from multiple sample with show_loci_legend = FALSE", {
+    p <- plot_vcf("HG00119_multi.vcf", show_loci_legend = F)
+    vdiffr::expect_doppelganger("multi show_loci_legend F", p)
+})
