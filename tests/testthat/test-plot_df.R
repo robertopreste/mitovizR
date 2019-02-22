3f6f49eee25a2193d146d4f9633a34f0530cafc3
Roberto Preste <roberto.preste@gmail.com>
2019-02-22 20:00:06 +0100

    Update tests

--- tests/testthat/test-plot_df.R
+++ tests/testthat/test-plot_df.R
@@ -99,7 +99,7 @@ test_that("plot_df returns correct plot with show_loci_legend = FALSE", {
     vdiffr::expect_doppelganger("show_loci_legend F", p)
 })
 
-test_that("plot_df returns correct plot with HF definition", {
+test_that("plot_df returns correct plot with HF", {
     df <- data.frame(CHROM = rep("chrMT", 16),
                      POS = c(750, 1438, 2706, 3106, 4216, 7028, 8935,
                              9389, 9899, 11251, 11719, 12633, 13368,
@@ -116,7 +116,7 @@ test_that("plot_df returns correct plot with HF definition", {
     vdiffr::expect_doppelganger("no args HF", p)
 })
 
-test_that("plot_df returns correct plot with HF definition as characters", {
+test_that("plot_df returns correct plot with HF as characters", {
     df <- data.frame(CHROM = rep("chrMT", 16),
                      POS = c(750, 1438, 2706, 3106, 4216, 7028, 8935,
                              9389, 9899, 11251, 11719, 12633, 13368,
@@ -131,6 +131,40 @@ test_that("plot_df returns correct plot with HF definition as characters", {
                      stringsAsFactors = F)
     p <- plot_df(df)
     vdiffr::expect_doppelganger("no args HF char", p)
+})
+
+test_that("plot_df returns correct plot with HF and hf_col", {
+    df <- data.frame(CHROM = rep("chrMT", 16),
+                     POS = c(750, 1438, 2706, 3106, 4216, 7028, 8935,
+                             9389, 9899, 11251, 11719, 12633, 13368,
+                             14766, 15326, 16294),
+                     REF = c("A", "A", "A", "CN", "T", "C", "C", "A",
+                             "T", "A", "G", "C", "G", "C", "A", "C"),
+                     ALT = c("G", "G", "G", "C", "C", "T", "T", "G",
+                             "C", "G", "A", "A", "A", "T", "G", "T"),
+                     hf_val = c(0.100, 0.200, 0.300, 0.400, 0.420, 0.500,
+                                0.600, 0.700, 0.800, 0.900, 1.000, 0.250,
+                                0.630, 0.810, 0.990, 0.120),
+                     stringsAsFactors = F)
+    p <- plot_df(df, hf_col = "hf_val")
+    vdiffr::expect_doppelganger("hf_col", p)
+})
+
+test_that("plot_df returns correct plot with HF as character and hf_col", {
+    df <- data.frame(CHROM = rep("chrMT", 16),
+                     POS = c(750, 1438, 2706, 3106, 4216, 7028, 8935,
+                             9389, 9899, 11251, 11719, 12633, 13368,
+                             14766, 15326, 16294),
+                     REF = c("A", "A", "A", "CN", "T", "C", "C", "A",
+                             "T", "A", "G", "C", "G", "C", "A", "C"),
+                     ALT = c("G", "G", "G", "C", "C", "T", "T", "G",
+                             "C", "G", "A", "A", "A", "T", "G", "T"),
+                     hf_val = c("0.100", "0.200", "0.300", "0.400", "0.420", "0.500",
+                                "0.600", "0.700", "0.800", "0.900", "1.000", "0.250",
+                                "0.630", "0.810", "0.990", "0.120"),
+                     stringsAsFactors = F)
+    p <- plot_df(df, hf_col = "hf_val")
+    vdiffr::expect_doppelganger("hf_col char", p)
 })
 
 # test_that("plot_df returns correct plot with show_var_labels = TRUE", {
--- tests/testthat/test-plot_vcf.R
+++ tests/testthat/test-plot_vcf.R
@@ -1,31 +1,31 @@
 context("Creating plot from VCF file")
 
 test_that("plot_vcf returns correct plot without arguments", {
-    p <- plot_vcf("HG00119.vcf")
+    p <- plot_vcf("sample_vcf.vcf")
     vdiffr::expect_doppelganger("no args", p)
 })
 
 test_that("plot_vcf returns correct plot with show_loci_names = FALSE", {
-    p <- plot_vcf("HG00119.vcf", show_loci_names = FALSE)
+    p <- plot_vcf("sample_vcf.vcf", show_loci_names = FALSE)
     vdiffr::expect_doppelganger("show_loci_names F", p)
 })
 
 test_that("plot_vcf returns correct plot with show_loci_legend = FALSE", {
-    p <- plot_vcf("HG00119.vcf", show_loci_legend = FALSE)
+    p <- plot_vcf("sample_vcf.vcf", show_loci_legend = FALSE)
     vdiffr::expect_doppelganger("show_loci_legend F", p)
 })
 
 test_that("plot_vcf returns correct plot from multiple samples", {
-    p <- plot_vcf("HG00119_multi.vcf")
+    p <- plot_vcf("sample_multi.vcf")
     vdiffr::expect_doppelganger("multi no args", p)
 })
 
 test_that("plot_vcf returns correct plot from multiple sample with show_loci_names = FALSE", {
-    p <- plot_vcf("HG00119_multi.vcf", show_loci_names = F)
+    p <- plot_vcf("sample_multi.vcf", show_loci_names = F)
     vdiffr::expect_doppelganger("multi show_loci_names F", p)
 })
 
 test_that("plot_vcf returns correct plot from multiple sample with show_loci_legend = FALSE", {
-    p <- plot_vcf("HG00119_multi.vcf", show_loci_legend = F)
+    p <- plot_vcf("sample_multi.vcf", show_loci_legend = F)
     vdiffr::expect_doppelganger("multi show_loci_legend F", p)
 })
